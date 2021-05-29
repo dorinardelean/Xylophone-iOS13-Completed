9509560aeaba09dd122e1f04ba19ed6c704d923f
Dorin Ardelean <ardeleandorin@gmail.com>
2021-05-30 00:52:20 +0300

    Change behavior of buttons and refactor some code.

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '13.0'
+
+target 'Xylophone' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Xylophone
+pod 'Piano', '~> 1.8'
+
+end
--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -8,64 +8,51 @@
 
 import UIKit
 import AVFoundation
+import Piano
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var cButton: UIButton!
-    @IBOutlet weak var dButton: UIButton!
-    @IBOutlet weak var eButton: UIButton!
-    @IBOutlet weak var fButton: UIButton!
-    @IBOutlet weak var gButton: UIButton!
-    @IBOutlet weak var aButton: UIButton!
-    @IBOutlet weak var bButton: UIButton!
-    @IBOutlet weak var cTopButton: UIButton!
+    @IBOutlet var keys: [UIButton]!
     
     var player: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupButtons()
+        
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
         playSound(soundName: sender.currentTitle!)
-        
+
         //Animate the button when pressed
-        UIView.animate(withDuration: 0.1) {
+        UIView.animate(withDuration: 0.03) {
             sender.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
         } completion: { _ in
             UIView.animate(withDuration: 0.3) {
                 sender.transform = CGAffineTransform.identity
             }
         }
-        
-        //Reduces the sender's (the button that got pressed) opacity to half.
-        sender.alpha = 0.5
-        
-        //Code should execute after 0.2 second delay.
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            //Bring's sender's opacity back up to fully opaque.
-            sender.alpha = 1.0
-        }
-        
+
     }
     
     func playSound(soundName: String) {
-        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
-        player = try! AVAudioPlayer(contentsOf: url!)
-        player.play()
+//        You need to put back the Sounds folder and add the wav files for this to work.
+//        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+//        player = try! AVAudioPlayer(contentsOf: url!)
+//        player.play()
+
+        🎹.play([
+            .sound(.asset(name: soundName)),
+            .hapticFeedback(.impact(.light))
+            ])
         
     }
     
     func setupButtons() {
-        cButton.layer.cornerRadius = 5
-        dButton.layer.cornerRadius = 5
-        eButton.layer.cornerRadius = 5
-        fButton.layer.cornerRadius = 5
-        gButton.layer.cornerRadius = 5
-        aButton.layer.cornerRadius = 5
-        bButton.layer.cornerRadius = 5
-        cTopButton.layer.cornerRadius = 5
+        for key in keys {
+            key.layer.cornerRadius = 5
+        }
     }
     
 }
